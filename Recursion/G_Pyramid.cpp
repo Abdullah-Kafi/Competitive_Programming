9b6aee2a0eabb39b17104d70c1eb3ac98ac24ad6
Abdullah Kafi <abdullahkafichowdhury@gmail.com>
2025-11-23 22:19:26 +0600

    solved A-G from Sheet #7

---
+++ Recursion/G_Pyramid.cpp
@@ -0,0 +1,56 @@
+///In the name of ALLAH, The Most Gracious and The Most Merciful
+///Praise be to ALLAH
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int sz = 0;
+int ar[sz];
+using ll = long long;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int, int>> vpii;
+typedef pair<int, int> pii;
+typedef vector<bool> vbl;
+typedef vector<pair<ll, ll>> vpll;
+typedef pair<ll, ll> pll;
+#define mem(var, x) memset((var), (x), sizeof(var))
+#define PQ priority_queue
+#define ff first
+#define ss second
+#define pb push_back
+#define MP make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define el "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define testCase int __; cin >> __; while(__--)
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int cnt;
+
+void Pyramid(int n)
+{
+    if(n == 0) return;
+    Pyramid(n - 1);
+
+    for(int i = n; i < cnt; i++) cout << " ";
+    for(int i = 0; i < 2 * (n - 1) + 1; i++) cout << "*";
+    cout << el;
+}
+
+void solve()
+{
+    cin >> cnt;
+    Pyramid(cnt);
+}
+int main()
+{
+    optimize();
+    
+    //testCase
+        solve();
+
+    return 0;
+}
